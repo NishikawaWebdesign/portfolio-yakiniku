6916b1626fe96582936521a9f633807e1aa9995a
NishikawaWebdesign <nishikawwwa.webdesign@gmail.com>
2026-02-23 11:27:41 +0900

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/https-nishikawa-portfolio-yakiniku/', // ←GitHubリポジトリ名
   plugins: [react()],
 })
